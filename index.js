cb9ee4708f42d5f08531400d1ade4e3b7adc3673
andrew <andrew2764@gmail.com>
2023-10-12 15:46:58 +0800

    Use kanye rest api instead

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const title = document.querySelector(".title");
-const advice = document.querySelector(".advice");
+const quote = document.querySelector(".quote");
 const rollBtn = document.querySelector(".roll");
-const url = "https://api.adviceslip.com/advice";
+const url = "https://api.kanye.rest";
 
 rollBtn.addEventListener("click", async () => {
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data.slip);
-  title.innerHTML = `Advice #${data.slip.id}`;
-  advice.innerHTML = data.slip.advice;
+  console.log(data.quote);
+  quote.innerHTML = data.quote;
 });
